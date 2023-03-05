b1c65fb39bd05886fbb7b6cef36240c7f17be88e
ayumedinaprameswari <84961344+zichae@users.noreply.github.com>
2023-03-05 23:01:12 +0700

    task 5 python
    square root calculate math

---
+++ task5_squareroot.py
@@ -0,0 +1,10 @@
+#cara dengan function import math
+import math
+x = int(input("Masukkan angka yang ingin dihitung: "))
+print (math.sqrt(x))
+
+#cara manual
+y = int(input("Masukkan angka yang ingin di hitung: "))
+x = y**2
+
+print("akar kuadrat dari ", x, "adalah", y)
