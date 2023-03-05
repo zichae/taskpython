689dfe28536a860665ac42a5350901d6f4161148
ayumedinaprameswari <84961344+zichae@users.noreply.github.com>
2023-03-05 23:04:45 +0700

    task 8 python
    switch variable from 2 input

---
+++ task8_changevariable.py
@@ -0,0 +1,15 @@
+binatang = ['anjing','kucing','semut']
+tumbuhan = ['cabai','timun','wortel']
+
+print("awalnya")
+print(f'binatang = {binatang}, tumbuhan = {tumbuhan}')
+
+#variabel tambahan/bantuan
+jenis_boneka = binatang
+
+#tukar
+binatang = tumbuhan
+tumbuhan = jenis_boneka
+
+print("hasilnya")
+print(f'binatang = {binatang}, tumbuhan = {tumbuhan}')
